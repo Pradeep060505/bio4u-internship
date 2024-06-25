ba504676ccabf8fe2b591015ed33572d9a22ed6b
Pradeep060505 <pradeeprathod71119@gmail.com>
2024-06-25 11:30:55 +0530

    "internship"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
+import React ,{useState,useEffect} from 'react'
 import './App.css';
-
+import NewLanding from './NewLanding';
+import About from './About';
+import Loader from './Loader';
+import Centre from './Centre';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Normal from './Normal';
+// import NewLoader from './NewLoader';
 function App() {
+  const [screenLoading, setScreenLoading] = useState(false);
+
+  useEffect(() => {
+    setScreenLoading(true);
+    setTimeout(() => {
+      setScreenLoading(false);
+    }, 2000);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      {screenLoading ? (
+        <Loader/>
+      ) :(
+        <>
+      
+     
+      <BrowserRouter>
+      <Routes>
+      <Route path='/normal' element={<Normal />}/>
+      <Route path='/' element={<NewLanding/>}/>
+      <Route path='/centre' element={<Centre/>}/>
+      <Route path='/about' element={<About/>}/>
+     
+      
+      </Routes>
+     
+      </BrowserRouter>
+      
+      
+     
+      
+      
+        </>
+      
+    )}
+   
+     
+</div>
   );
 }
 
